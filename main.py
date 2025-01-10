79ac4721b4fe8cc632c79d736546c19665185d45
DavidWithChorizo <zceelhe@ucl.ac.uk>
2025-01-10 22:54:45 +0800

    Initiated the codes, and established the type of models and the structure of the main.py code.

---
+++ main.py
@@ -0,0 +1,5 @@
+# This is the main file for the project. It will be used to run the project. 
+# There will be two models being trained for the first part and of the project, and two for the latter part.
+
+#---------------------------------------------------- Task 1, Model 1 ----------------------------------------------------#
+
